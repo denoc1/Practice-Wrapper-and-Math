59a8b887cabef23047ccddcf524e99c32bdf3ed4
denoc1 <176614645+denoc1@users.noreply.github.com>
2024-10-01 13:27:05 +0000

    Practice in class

---
+++ wrapPractice.java
@@ -0,0 +1,17 @@
+public class wrapPractice{
+
+
+    public static void main(String[] args){
+
+        System.out.println("Math.abs(-1.23):" + Math.abs(-1.23));
+        System.out.println("Math.pow(3, 2):" + Math.pow(3, 2));
+        System.out.println("Math.pow(10, -2):" + Math.pow(10, -2));
+        System.out.println("Math.sqrt(121.0):" + Math.sqrt(121.0));
+
+        // System.out.println(Integer.MIN_VALUE);
+        // System.out.println(Integer.MAX_VALUE);
+        // System.out.println(Integer.MIN_VALUE - 1);   //underflow
+        // System.out.println(Integer.MAX_VALUE + 1);   //overflow
+
+    }
+}
